632c30f19653f4587463ad19b0e898949b837d4f
mohamedsaeed22 <mohamed.saeed1006@gmail.com>
2025-06-11 08:35:45 +0300

    fix: navbar user error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = async () => {
         <div className="flex flex-col">
           <span className="text-xs leading-3 font-medium">John Doe</span>
           <span className="text-[10px] text-gray-500 text-right">
-            {user?.publicMetadata?.role as string}
+            {/* {user?.publicMetadata?.role as string} */}
           </span>
         </div>
         {/* <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/> */}
